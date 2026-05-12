a53b34bad1fcbc1002ef428864177d24fe9d5c63
AI & Agentic AI - Python Developer <pydevgithub@gmail.com>
2026-05-12 15:12:38 +0530

    update

---
+++ eslint.config.js
@@ -0,0 +1,33 @@
+const nextPlugin = require('@next/eslint-plugin-next');
+const tsParser = require('@typescript-eslint/parser');
+const tsPlugin = require('@typescript-eslint/eslint-plugin');
+
+module.exports = [
+  {
+    ignores: ['.next/**', 'node_modules/**', 'out/**', '.git/**'],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      globals: {
+        React: 'readonly',
+      },
+    },
+    plugins: {
+      '@next/next': nextPlugin,
+      '@typescript-eslint': tsPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs['core-web-vitals'].rules,
+      ...tsPlugin.configs.recommended.rules,
+    },
+  },
+];
